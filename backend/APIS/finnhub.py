20b80de84e9e46557cc35be89bc790a11b5ca99c
Casen Ward <casenward@gmail.com>
2025-09-15 18:52:21 -0400

    Remove .env from repo history

--- backend/APIS/finnhub.py
+++ backend/APIS/finnhub.py
@@ -20,8 +20,16 @@ def get_quote(symbol):
     r.raise_for_status()
     return r.json()
 
+# Get the current metrics for a stock
+def get_metrics(symbol):
+    url = f"{URL}/stock/metric"
+    params = {"symbol": symbol, "metric": "all", "token": FINNHUB_API_KEY}
+    r = requests.get(url, params=params)
+    r.raise_for_status()
+    return r.json()
 
 
 if __name__ == "__main__":
     print(get_quote("AAPL"))
+    print(get_metrics("AAPL"))
 
