9093f34690d82af7cdb7d75324b42f0ab3e51e5c
Casen Ward <casenward@gmail.com>
2025-09-24 20:30:33 -0400

    Version 1

--- backend/stock.py
+++ backend/stock.py
@@ -3,53 +3,21 @@
 
 import backend.apis.finnhub as finnhub
 import backend.services.score_calculator as score_calculator
+import yfinance as yahoo
 
 
 def final_rating(score: float) -> str:
-    if score >= 85:
+    if score >= 80:
         return "Strong Buy"
-    elif score >= 70:
+    elif score >= 65:
         return "Buy"
-    elif score >= 55:
+    elif score >= 45:
         return "Hold"
-    elif score >= 40:
+    elif score >= 25:
         return "Sell"
     else:
         return "Strong Sell"
 
-#DJIA 30 stocks for testing purposes
-DJIA_30 = {
-    "MMM": "3M Company",
-    "AXP": "American Express Company",
-    "AAPL": "Apple Inc.",
-    "BA": "Boeing Company",
-    "CAT": "Caterpillar Inc.",
-    "CVX": "Chevron Corporation",
-    "CSCO": "Cisco Systems, Inc.",
-    "KO": "The Coca-Cola Company",
-    "DOW": "Dow Inc.",
-    "GS": "Goldman Sachs Group, Inc.",
-    "HD": "The Home Depot, Inc.",
-    "IBM": "International Business Machines Corporation",
-    "INTC": "Intel Corporation",
-    "JNJ": "Johnson & Johnson",
-    "JPM": "JPMorgan Chase & Co.",
-    "MCD": "McDonald's Corporation",
-    "MRK": "Merck & Co., Inc.",
-    "MSFT": "Microsoft Corporation",
-    "NKE": "Nike, Inc.",
-    "PG": "Procter & Gamble Company",
-    "CRM": "Salesforce, Inc.",
-    "TRV": "The Travelers Companies, Inc.",
-    "UNH": "UnitedHealth Group Incorporated",
-    "VZ": "Verizon Communications Inc.",
-    "V": "Visa Inc.",
-    "WBA": "Walgreens Boots Alliance, Inc.",
-    "WMT": "Walmart Inc.",
-    "DIS": "The Walt Disney Company",
-    "AMGN": "Amgen Inc.",
-    "HON": "Honeywell International Inc.",
-}
 
 class Stock:
     def __init__(self, symbol):
@@ -63,8 +31,9 @@ class Stock:
         self.symbol = symbol.upper() 
 
     def set_name(self):
-        self.name = DJIA_30.get(self.symbol, "Unknown Company")  # Default to "Unknown Company" if symbol not found
-    
+        ticker = yahoo.Ticker(self.symbol)
+        self.name = ticker.info.get("longName", "Unknown Company")
+
     def set_currentPrice(self):
         self.current_price = finnhub.get_quote(self.symbol)["c"] # Current price is in the "c" field of the quote response
     
